1713530b89a542713fe8d9d7611a40fb70f0822e
eecevit-tgm <eecevit@student.tgm.ac.at>
2019-01-25 02:45:07 +0100

    tests endlich ready

--- src/unittest/python/GK/test_read.py
+++ src/unittest/python/GK/test_read.py
@@ -18,7 +18,7 @@ def run_around_tests():
     file = open('user.json', "w+")
     file.write('[{"username": "eecevit", "email": "eecevit@student.tgm.ac.at", "picture": "ds"}]')
 
-def test_readall(client):
+def test_readAll(client):
     res = client.get('/users')
     assert res.json == [{"username": "eecevit", "email": "eecevit@student.tgm.ac.at", "picture": "ds"}]
 
@@ -28,18 +28,39 @@ def test_singleUser(client):
     assert res.json == [{"username": "eecevit", "email": "eecevit@student.tgm.ac.at", "picture": "ds"}]
 
 def test_addUser(client):
-    client.post("users", json=user01)
+    client.post("/users", json=user01)
     res = client.get("/users/test01")
-    print(res.data)
-    assert (res.json['username'] == "test01") and (res.json['email'] == "test01@student.tgm.ac.at") and (res.json['picture'] == "Tester0123")
+    res02 = res.json[0]
+    assert (res02['username'] == "test01") and (res02['email'] == "test01@student.tgm.ac.at") and (res02['picture'] == "Tester0123")
+    client.delete("/users/test01")
 
 
-def test_update_user(client):
+def test_addMultiple(client):
+    client.post("/users", json=user01)
+    client.post("/users", json=user02)
+    res01 = client.get("/users/test01").json[0]
+    res02 = client.get("/users/test02").json[0]
+    assert (res01['username'] == "test01") and (res01['email'] == "test01@student.tgm.ac.at") and (res01['picture'] == "Tester0123") and\
+            (res02['username'] == "test02") and (res02['email'] == "test02@student.tgm.ac.at") and (res02['picture'] == "Tester0123")
+    client.delete("/users/test01")
+    client.delete("/users/test02")
+
+
+
+def test_updateUser(client):
     client.put('/users/eecevit', json=user02)
-    res = client.get('/users/eecevit')
-    assert b'{"username":"test02", "email":"test02@student.tgm.ac.at", "picture":"Tester0123"}\n' in res.data
+    res = client.get('/users/test02')
+    res02 = res.json[0]
+    assert (res02['username'] == "test02") and (res02['email'] == "test02@student.tgm.ac.at") and (res02['picture'] == "Tester0123")
 
-def test_delete_user(client):
+def test_deleteUser(client):
     client.delete('/users/eecevit')
     res=client.get('/users/eecevit')
-    assert b'{"message": "User {} doesn\'t exist.' in res.data
+    print(res.data)
+    assert b'{"message": "User {} doesn\'t exist"}\n' in res.data
+
+def test_deleteUser02(client):
+    client.post("/users", json=user01)
+    client.delete("/users/user01")
+    res = client.get("/users")
+    print(res.data)
